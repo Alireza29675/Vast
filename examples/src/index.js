b04272e14843ba9318d0a4cbbf3a9df99448aa67
Alireza Sheikholmolouki <alireza.sheikholmolouki@gmail.com>
2018-08-17 19:12:17 +0430

    View is being created

--- examples/src/index.js
+++ examples/src/index.js
@@ -1,3 +1,4 @@
 const Vast = require('../../lib/index')
-
 const vast = new Vast();
+
+document.body.appendChild(vast.view);
--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
 class Vast {
 
     constructor () {
-
+        this.__createView();
     }
-    
+
+    __createView () {
+        this.view = document.createElement('canvas');
+        this.__setSize();
+        window.addEventListener('resize', this.__setSize.bind(this));
+    }
+
+    __setSize () {
+        this.view.width = this.width = window.innerWidth;
+        this.view.height = this.height = window.innerWidth;
+    }
+
 }
 
 export default Vast;
