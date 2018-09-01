825a8d0abacf1a0ecc3ba9f9268a19f3cab46b22
Pouya MozaffarMagham <pouya.mozafar@gmail.com>
2018-09-01 14:34:49 +0430

    showSpiral Property and drawSpiral added to VastObject

--- src/components/VastObject.js
+++ src/components/VastObject.js
@@ -1,5 +1,7 @@
 class VastObject {
 
+    showSpiral = false;
+    
     spiral ={
         minX:0,
         minY:0,
@@ -14,6 +16,9 @@ class VastObject {
     __changes () {};
     __draw () {};
     __calculateSpiral(){};
+    __drawSpiral(){
+        this.ctx.strokeRect(this.spiral.minX-2, this.spiral.minY-2, this.spiral.maxX - this.spiral.minX + 4, this.spiral.maxY - this.spiral.minY+4);
+    }
 }
 
 module.exports = VastObject;
