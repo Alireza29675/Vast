d0423fc8570825223113600876c874dcef51e627
Pouya MozaffarMagham <pouya.mozafar@gmail.com>
2018-09-01 14:11:06 +0430

    calcuate spiral method and spiral array added to VastObject

--- src/components/VastObject.js
+++ src/components/VastObject.js
@@ -1,10 +1,14 @@
 class VastObject {
+
+    spiral = [];
+
     __setVast (vast) {
         this.vast = vast;
         this.ctx = vast.ctx;
     }
     __changes () {};
     __draw () {};
+    __calculateSpiral(){};
 }
 
 module.exports = VastObject;
